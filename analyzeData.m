a06444151f0f2d1557354a8768ae91f62087b513
nenetto <nenetto@gmail.com>
2014-07-22 12:19:19 +0200

    Remove the Tuckey depth for low performance and consistence

--- analyzeData.m
+++ analyzeData.m
@@ -8,16 +8,16 @@ function analyzeData(fileData)
 
     %% Extract the mean and median
     normal_mean = mean(OptData(:,:,1:1000),3);
-    OptDataTukey = medianTukey(OptData(:,:,1001:end));
+    % OptDataTukey = medianTukey(OptData(:,:,1001:end));
     OptMahalanobisMean = mahalanobis_mean_extraction(OptData(:,:,1:1000));
     OptDataSDFirst1000  = std(OptData(:,:,1:1000),0,3);
     OptDataSDLast1000  = std(OptData(:,:,1001:end),0,3);
 
     %% Save data
 
-    % AbbData3, NormalMean3, MedianTukey3, MahalanobisMean3
+    % AbbData3, NormalMean3,, MahalanobisMean3
 
-    data = cat(2,AbbData,normal_mean,OptDataTukey,OptMahalanobisMean,OptDataSDFirst1000,OptDataSDLast1000);
+    data = cat(2,AbbData,normal_mean,OptMahalanobisMean,OptDataSDFirst1000,OptDataSDLast1000);
     fileName_cut  = regexprep(fileData, 'DATA.mat', '');
     save(fileName_cut,'data');
 
